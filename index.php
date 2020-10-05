8b3f94307960335976f5224a1d1b17b0d2f24091
Ryan Guest <rguest20@googlemail.com>
2020-10-05 11:37:44 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Genuine Fakes Database</title>
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+    <div class="nav">
+      <h3 class="logo">Genuine Fakes</h3>
+    </div>
+    <div class="title">
+       <h1 class="maintitle">Picture Database<h1>
+    </div>
+    <div class="forms">
+      <h3>Picture Search</h3>
+      <form class="options" action="index.html" method="post">
+        <input type=text class="query"> <br />
+        <button>Search For a Picture</button>
+      </form>
+      <h3>Picture Upload</h3>
+      <form class="" action="index.html" method="post">
+        <input type=file class="pictureupload"></input><br /  >
+        <button>Input a New Picture</button>
+      </form>
+  </div>
+    <div class="footer">
+      <p>Made by Ryan Guest 2020</p>
+    </div>
+  </body>
+</html>
